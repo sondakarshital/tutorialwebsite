045cb90e06cfa43b5c208500928fa94f93543057
shital <sondakarshital@gmail.com>
2019-02-03 16:59:03 +0530

    last commit

--- src/app/express-js/express-js-routing.module.ts
+++ src/app/express-js/express-js-routing.module.ts
@@ -9,21 +9,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
- { 
-	  path: 'expressJs',
-          component: ExpressJsComponent,
-          children: [ 
-	    {
-	       path: 'introduction-to-expressJs',
-	       component: EFirstTutorialComponent
-	   },
-	   {
-	       path: 'setting-up-nodejs-environment',
-	       component: ESecondTutorialComponent
-	   }
+//  { 
+// 	  path: 'expressJs',
+//           component: ExpressJsComponent,
+//           children: [ 
+// 	    {
+// 	       path: 'introduction-to-expressJs',
+// 	       component: EFirstTutorialComponent
+// 	   },
+// 	   {
+// 	       path: 'setting-up-nodejs-environment',
+// 	       component: ESecondTutorialComponent
+// 	   }
 			
-	   ]
-	}
+// 	   ]
+// 	}
+{ path:'expressJs/introduction-to-expressJs',component:EFirstTutorialComponent}
 ];
 
 @NgModule({
